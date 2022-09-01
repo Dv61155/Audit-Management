666dd84dbdde92721b66ed144a626fbae484075a
dheeraj.vishwakarma@cognizant.com <dheeraj.vishwakarma@cognizant.com>
2022-09-01 22:55:20 +0530

    almost

--- audit-checklist/src/test/java/com/mfpe/service/QuestionServiceTest.java
+++ audit-checklist/src/test/java/com/mfpe/service/QuestionServiceTest.java
@@ -9,6 +9,7 @@ import java.util.List;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import com.mfpe.model.AuditType;
@@ -21,7 +22,7 @@ class QuestionServiceTest {
 	@Mock
 	QuestionRepo repo;
 	
-	@InjectMocks
+	@Autowired
     QuestionServiceImpl impl;
 	
 	@Test
@@ -31,10 +32,16 @@ class QuestionServiceTest {
 	
 	@Test
 	public  void getQuestionsByAuditType(){
-		List<Question> auditQuestion = new ArrayList<>();
-		auditQuestion.add(new Question(1,"Question","AuditType","response"));
-		when(repo.findAll()).thenReturn(auditQuestion);
-		assertEquals(auditQuestion, impl.getQuestionsByAuditType(new AuditType("AuditType")));
+	//	AuditType type=new AuditType("Internal");
+		List<Question> questions = new ArrayList<>();		
+		questions.add(new Question(1,"Have all Change requests followed SDLC before PROD move?","Internal","No"));
+		questions.add(new Question(2,"Have all Change requests been approved by the application owner?","Internal","No"));
+		questions.add(new Question(3,"Are all artifacts like CR document, Unit test cases available?","Internal","No"));
+		questions.add(new Question(4,"Is the SIT and UAT sign-off available?","Internal","No"));
+		questions.add(new Question(5,"Is data deletion from the system done with application owner approval?","Internal","No"));
+	
+		when(repo.findAll()).thenReturn(questions);
+		assertEquals(questions,impl.getQuestionsByAuditType(new AuditType("Internal")));
 	}
 
 }
