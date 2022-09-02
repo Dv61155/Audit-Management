fb31e94cb06252d635b9bfeb916a19ca7b2a2263
rohitsa321 <rohit.sahu@cognizant.com>
2022-09-02 21:46:36 +0530

    status column added

--- frontend/frontend/src/App.js
+++ frontend/frontend/src/App.js
@@ -50,7 +50,7 @@ function App() {
             }/>
             <Route path="/auditResponse" element={
                 <Layout>
-                    <Navbar/>
+                    <NavbarPage/>
                     <AuditResponse/>
                 </Layout>
             }/>
